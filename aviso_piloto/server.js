5c54fe555d359729bd084b55bf45b868597a0534
mike <mike58072@gmail.com>
2019-09-01 22:52:24 -0600

    agregando cambios a script de construccion

--- aviso_piloto/server.js
+++ aviso_piloto/server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = 3001,
+  port = 3003,
   bodyParser = require('body-parser');
 
   app.use(bodyParser.urlencoded({ extended: true }));
